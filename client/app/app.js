d6ff565838cada65541c6736131802833e44a1b4
Vontez Donaby <vdonaby@gmail.com>
2016-01-08 22:23:55 -0600

    first commit

---
+++ client/app/app.js
@@ -0,0 +1,18 @@
+'use strict';
+
+angular.module('easyEatsApp', [
+  'easyEatsApp.constants',
+  'ngCookies',
+  'ngResource',
+  'ngSanitize',
+  'ngRoute',
+  'ui.bootstrap'
+])
+  .config(function($routeProvider, $locationProvider) {
+    $routeProvider
+      .otherwise({
+        redirectTo: '/'
+      });
+
+    $locationProvider.html5Mode(true);
+  });
